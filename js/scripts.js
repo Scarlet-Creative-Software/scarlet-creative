5272f2424acd93167f93ad7ae56275924e8f9b48
Gabriela Sewdhan <gabiems13@gmail.com>
2022-09-30 23:30:07 -0400

    Styling

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,20 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Loading icon
-    document.onreadystatechange = function() {
-        if (document.readyState !== "complete") {
-            document.querySelector(
-              "body").style.visibility = "hidden";
-            document.querySelector(
-              ".spinner").style.visibility = "visible";
-        } else {
-            document.querySelector(
-              ".spinner").style.display = "none";
-            document.querySelector(
-              "body").style.visibility = "visible";
-        }
-    };
+    // // Loading icon
+    // document.onreadystatechange = function() {
+    //     if (document.readyState !== "complete") {
+    //         document.querySelector(
+    //           "body").style.visibility = "hidden";
+    //         document.querySelector(
+    //           ".spinner").style.visibility = "visible";
+    //     } else {
+    //         document.querySelector(
+    //           ".spinner").style.display = "none";
+    //         document.querySelector(
+    //           "body").style.visibility = "visible";
+    //     }
+    // };
 
     // Navbar shrink function
     var navbarShrink = function () {
