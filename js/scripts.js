ba5b42d10cfc1622b93ed0b827acd6b17a40967e
Gabriela Sewdhan <gabiems13@gmail.com>
2022-10-13 03:54:25 -0400

    Updating quote popup content and responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,13 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // // Loading icon
-    // document.onreadystatechange = function() {
-    //     if (document.readyState !== "complete") {
-    //         document.querySelector(
-    //           "body").style.visibility = "hidden";
-    //         document.querySelector(
-    //           ".spinner").style.visibility = "visible";
-    //     } else {
-    //         document.querySelector(
-    //           ".spinner").style.display = "none";
-    //         document.querySelector(
-    //           "body").style.visibility = "visible";
-    //     }
-    // };
+    // Loading icon
+    // $(window).on('load', function () {
+    //     setTimeout(() => {
+    //         $('.boxLoading').hide();
+    //         document.querySelector('.content-after-load').style.opacity = "1";
+    //     }, 0);
+    //   });
 
     // Navbar shrink function
     var navbarShrink = function () {
